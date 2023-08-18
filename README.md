99e8f6ed1f21ac687bcd10c1ac98b1b048c3fcac
Cesarbautista10 <cesar.bautista@uelectronics.com>
2023-08-18 16:09:52 -0600

    config Readme and add oled_hola_mundo

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# OLED_ch552
+
+# Configuración  OLED I2C SSD1306
+En la convergencia de la innovación tecnológica y la creatividad visual, la configuración de pantallas OLED adquiere un papel central. En este contexto, el presente texto se adentra en el proceso de configuración de un modelo específico: la pantalla OLED SSD1306. Con el propósito de lograr una comunicación fluida y eficiente, se recurre a la utilización del protocolo I2C y al entorno de programación Arduino IDE. Este repositorio explora los pasos esenciales para llevar a cabo dicha configuración, haciendo hincapié en la obtención del archivo 'oled_hola_mundo', la adaptación precisa de los parámetros de datos en sintonía con el entorno ch55xduino y la consecuente posibilidad de presentar mensajes de manera efectiva en la pantalla. Adentrémonos en esta travesía de integración tecnológica y expresión visual.
+# Configurando el entorno! 👋
+Para configurar nuestra pantalla OLED, hacemos uso del Arduino IDE con el uso del pryecto ch55xduino para subir los archivos.
+Para poder iniciar con nuestro despliesgue:
+
+1. Descarga el archivo oled_hola_mundo
+2. Dentro de la carpeta encontraras un archivo que lleva por nombre "oled_hola_mundo.ino" abre el archivo con Arduino IDE.
+3. Para poder cargar el archivo configura el entorno con las siguiente especificaciones:
+
+-Reloj a 16 Mhz.
+
+-Metodo de carga USB.
+
+*Ajuste de carga a "USER CODE /w 2866 USB RAM"
+
+4. Para completar la carga del codigo, presiona el boton de boot mientras se conecta via USB. 
+5. Carga el codigo. 
+
+
+
+## Screenshots
+
+![imaget](./img/oled.jpg)
+
---
+++ oled_hola_mundo/oled_hola_mundo.c
@@ -0,0 +1,66 @@
+
+
+// ===================================================================================
+// Libraries, Definitions and Macros
+// ===================================================================================
+
+// Libraries
+#include "src/config.h"                   // user configurations
+#include "src/system.h"                   // system functions
+#include "src/gpio.h"                     // for GPIO
+#include "src/delay.h"                    // for delays
+#include "src/oled.h"                     // for OLED
+#include "src/usb_cdc.h"                  // for USB-CDC serial
+
+// Prototypes for used interrupts
+void USB_interrupt(void);
+void USB_ISR(void) __interrupt(INT_NO_USB) {
+  USB_interrupt();
+}
+
+// ===================================================================================
+// Buzzer Function
+// ===================================================================================
+
+// Create a short beep on the buzzer
+void beep(void) {
+  uint8_t i;
+  for(i=255; i; i--) {
+    PIN_low(PIN_BUZZER);
+    DLY_us(125);
+    PIN_high(PIN_BUZZER);
+    DLY_us(125);
+  }
+}
+
+// ===================================================================================
+// Main Function
+// ===================================================================================
+
+void main(void) {
+  // Setup
+  CLK_config();                           // configure system clock
+  DLY_ms(5);                              // wait for clock to stabilize
+  CDC_init();                             // init USB CDC
+  OLED_init();                            // init OLED
+
+   // Print start message
+  OLED_print("*OLED UNITelectronics*");
+  OLED_print("---------------------");
+  OLED_print("Ready\n");
+  OLED_print("_\r");
+  OLED_print("OLED_ch552\n");
+  OLED_print("Hola Mundo!!!!!!!\n");
+  OLED_print("Ready\n");
+  beep();
+
+
+  // Loop
+  while(1) {
+    if(CDC_available()) {                 // something coming in?
+      char c = CDC_read();                // read the character ...
+      OLED_write(c);                      // ... and print it on the OLED
+      if((c == 10) || (c == 7)) beep();   // beep on newline command
+    }
+  }
+}
